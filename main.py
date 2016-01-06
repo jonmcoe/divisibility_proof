ff600257c94531daaa078cc7e8557a8e7823810d
Jon Coe <jonathon.coe@offerpop.com>
2016-01-05 23:58:37 -0500

    whitespace

--- main.py
+++ main.py
@@ -12,30 +12,30 @@ TODO: write this formally
 """
 
 EXPECT_TRUE = [ #TODO: generate this list
-	(28, 3),
-	(10, 9),
-	(10, 3),
-	(9, 8),
-	(9, 4),
-	(9, 2),
-	(5, 4),
-	(5, 2),
-	(4, 3),
-	(3, 2),
-	(2, 1)
+    (28, 3),
+    (10, 9),
+    (10, 3),
+    (9, 8),
+    (9, 4),
+    (9, 2),
+    (5, 4),
+    (5, 2),
+    (4, 3),
+    (3, 2),
+    (2, 1)
 ]
 EXPECT_FALSE = [
-	(10, 4),
-	(10, 27)
+    (10, 4),
+    (10, 27)
 ]
 RANGE = range(10**6)
 
 for case in EXPECT_TRUE + EXPECT_FALSE:
-	check_mod_match = make_mod_matcher(case[0], case[1])
-	match = all([check_mod_match(i) for i in RANGE])
-	str_dict = {
-		'b': case[0],
-		'd': case[1],
-		'm': match
-	}
-	print("Base {b}\t Divisor {d}: {m}".format(**str_dict))
+    check_mod_match = make_mod_matcher(case[0], case[1])
+    match = all([check_mod_match(i) for i in RANGE])
+    str_dict = {
+        'b': case[0],
+        'd': case[1],
+        'm': match
+    }
+    print("Base {b}\t Divisor {d}: {m}".format(**str_dict))
