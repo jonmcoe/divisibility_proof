f12638cc79e498c07a08c6b971749b8ba7ee5f5c
Jon Coe <jonathon.coe@offerpop.com>
2016-01-06 19:08:49 -0500

    generate integer roots correctly in main

--- main.py
+++ main.py
@@ -14,21 +14,22 @@ TODO: write this formally
 """
 
 MAX_BASE = 36
-				     # this shows approach to generating is unsound. 33,2 is excluded by below
-EXPECT_TRUE = [(2,1),(33,2)]+list(itertools.chain(
-	*[ #TODO: indentation?
-		[
-			(b, int((b-1)**(1/k)))
-			for k in itertools.takewhile(lambda i: ((b-1)**(1/i)).is_integer(), itertools.count(1))
-		]
-		for b in range(3,MAX_BASE)
-	]
+EXPECT_TRUE = [(2, 1)] + list(itertools.chain(
+    *[  # TODO: indentation?
+        [
+            (b, int((b - 1) ** (1 / k)))
+            for k in itertools.takewhile(lambda i: ((b - 1) ** (1 / i)) >= 2,
+                                         itertools.count(1))
+            if ((b - 1) ** (1 / k)).is_integer()
+        ]
+        for b in range(3, MAX_BASE)
+    ]
 ))
 EXPECT_FALSE = [
     (10, 4),
     (10, 27)
 ]
-N_RANGE = range(10**5)
+N_RANGE = range(10 ** 5)
 
 for case in EXPECT_TRUE + EXPECT_FALSE:
     check_mod_match = make_mod_matcher(case[0], case[1])
