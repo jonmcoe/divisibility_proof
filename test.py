fbcaa5f41e72cccc8ced04154a417bd8a0d31a94
Jon Coe <jonathon.coe@offerpop.com>
2016-01-06 19:03:21 -0500

    additional tests

--- test.py
+++ test.py
@@ -5,7 +5,28 @@ from util import *
 
 class UtilTestCase(TestCase):
 
-	def test_int_to_string(self):
-		self.assertEquals(int_to_string(10, 123), '123')
+    def test_int_to_string_decimal(self):
+        self.assertEquals(int_to_string(10, 123), '123')
 
-	# TODO: write more
+    def test_int_to_string_hex(self):
+        self.assertEqual(int_to_string(16, 123), '7B')
+
+    def test_sum_digits_decimal(self):
+        self.assertEqual(sum_digits('12345'), 15)
+
+    def test_sum_digits_hex(self):
+        self.assertEqual(sum_digits('3F'), 18)
+
+    def test_make_mod_matcher_decimal(self):
+        mod_matcher_true = make_mod_matcher(10, 3)
+        self.assertTrue(mod_matcher_true(457983452))
+
+        mod_matcher_false = make_mod_matcher(10, 7)
+        self.assertFalse(mod_matcher_false(34589343))
+
+    def test_make_mod_matcher_hex(self):
+        mod_matcher_true = make_mod_matcher(16, 15)
+        self.assertTrue(mod_matcher_true(457983452))
+
+        mod_matcher_false = make_mod_matcher(16, 7)
+        self.assertFalse(mod_matcher_false(34589343))
--- util.py
+++ util.py
@@ -8,22 +8,20 @@ MAX_BASE = len(DIGITS)
 
 
 def make_mod_matcher(base, divisor):
-	def f(n):
-		return n % divisor == sum_digits(int_to_string(base, n)) % divisor
-	return f
+    def f(n):
+        return n % divisor == sum_digits(int_to_string(base, n)) % divisor
+    return f
 
 
 def int_to_string(base, n):
-	ans = ""
-	if n == 0:
-		return '0'
-	while n > 0:
-		ans = DIGITS[n % base] + ans
-		n //= base
-	return ans
-
-
+    ans = ""
+    if n == 0:
+        return '0'
+    while n > 0:
+        ans = DIGITS[n % base] + ans
+        n //= base
+    return ans
 
 
 def sum_digits(s):
-	return sum([int(i, MAX_BASE) for i in s])
+    return sum([int(i, MAX_BASE) for i in s])
